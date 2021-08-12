5aefd617524f5b04c9b8c6ae45c6d13542467ba1
unknown <ryanhidayat074@gmail.com>
2021-08-12 13:10:48 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -17,3 +17,16 @@ exports.tampilsemuamahasiswa = function(req, res){
         }
     });
 };
+
+//menampilkan data mahasiswa berdasarkan id nya
+exports.tampilberdasarkanid = function(req, res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
+};
