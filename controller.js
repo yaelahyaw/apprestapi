e94b4eb2eb3de343d052e7b20b23d41f40639e1d
unknown <ryanhidayat074@gmail.com>
2021-08-12 13:51:53 +0700

    berhasil nambah data

--- controller.js
+++ controller.js
@@ -3,30 +3,46 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res){
+exports.index = function (req, res) {
     response.ok('Aplikasi REST API ku berjalan!', res);
 };
 
 //Menampilkan semua data mahasiswa
-exports.tampilsemuamahasiswa = function(req, res){
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
-        if(error){
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+        if (error) {
             connection.log(error);
-        }else{
-            response.ok(rows,res);
+        } else {
+            response.ok(rows, res);
         }
     });
 };
 
 //menampilkan data mahasiswa berdasarkan id nya
-exports.tampilberdasarkanid = function(req, res){
+exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
     connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
-    function(error, rows, fields){
-        if(error){
-            console.log(error);
-        }else{
-            response.ok(rows, res);
-        }
-    });
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+};
+
+//menambahkan data mahasiswa
+exports.tambahmahasiswa = function (req, res){
+    let nim = req.body.nim;
+    let nama = req.body.nama;
+    let jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)', [nim,nama,jurusan],
+        function(error,rows,fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok(rows, res);
+            }
+        });
 };
